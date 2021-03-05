164eca6e206e664b5051a3e7065e75655d24b514
Chenchen XU <chenchen.xu@aitmed.com>
2021-03-05 19:02:00 +0800

    update

--- app.js
+++ app.js
@@ -2,20 +2,18 @@ const { dir } = require('console')
 const express = require('express')
 const app = express()
 const fs = require('fs')
-var server = app.listen(3001,()=>{
-    let host = server.address().address
-    let port = server.address().port
-    console.log("应用实例，访问地址为 http://localhost:3001")
+var server = app.listen(3001, () => {
+    console.log(`listen on http://localhost:3001`)
 })
-let searchFromJson = (object,id) => {
-    return object.filter(o => o.fields.zip === id)[0].fields.geopoint; 
-  }
+let searchFromJson = (object, id) => {
+    return object.filter(option => { option.fields.zip === id})[0].fields.geopoint
+}
 
-app.get('/zipCode/Data',(req,res)=>{
-    fs.readFile( __dirname + "/" + "data.json", 'utf8', function (err, data) {
-        console.log('test');
-        data = JSON.parse( data );
-        let result = searchFromJson(data,req.query.id)
-        res.end( JSON.stringify(result));
+app.get('/zipCode/Data', (req, res) => {
+    fs.readFile('http://file.xccblog.cn/us-zip-code-latitude-and-longitude.json', 'utf8', function (err, data) {
+        if (err) console.error(err);
+        data = JSON.parse(data);
+        let result = searchFromJson(data, req.query.id)
+        res.end(JSON.stringify(result));
     });
 })
